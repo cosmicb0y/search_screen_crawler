2278f0b56d86d24b91953e0c76b6cc1c7e34a78c
Hyowon Jang <jang.hw73@gmail.com>
2018-11-23 13:26:00 +0900

    initial commit

---
+++ src/main/java/com/hackday/crawler/CrawlerApplication.java
@@ -0,0 +1,12 @@
+package com.hackday.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrawlerApplication.class, args);
+    }
+}
